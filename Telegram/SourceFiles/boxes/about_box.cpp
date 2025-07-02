c53370a7d8a0f43090a37afc7826dab1fff377ce
Revincx <revincx233@gmail.com>
2025-07-02 11:18:12 +0800

    fix: aboutbox is not a namespace

--- Telegram/SourceFiles/boxes/about_box.cpp
+++ Telegram/SourceFiles/boxes/about_box.cpp
@@ -56,10 +56,7 @@ rpl::producer<TextWithEntities> Text3() {
 } // namespace
 
 void AboutBox(not_null<Ui::GenericBox*> box) {
-	box->setTitle(rpl::single(u"Telegram Desktop"_q));
-
-void AboutBox::prepare() {
-	setTitle(rpl::single(u"Yukigram"_q));
+	box->setTitle(rpl::single(u"Yukigram"_q));
 	auto layout = box->verticalLayout();
 
 	const auto version = layout->add(
