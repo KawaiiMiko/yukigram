44adce62d36dfe22d80a97b482a9e6ada73a70d8
Revincx <revincx233@gmail.com>
2026-04-18 21:11:04 +0800

    chore: switch order of HideMessage and ViewAsJson

--- Telegram/SourceFiles/settings.h
+++ Telegram/SourceFiles/settings.h
@@ -153,8 +153,8 @@ DeclareSetting(EnhancedSetting, EnhancedOptions);
 
 enum class ExtraContextMenuOption : int {
 	Repeater = 1,
-	ViewAsJson = 2,
-	HideMessage = 3,
+	HideMessage = 2,
+	ViewAsJson = 3,
 };
 
 DeclareSetting(int, NetRequestsCount);
