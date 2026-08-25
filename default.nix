bd704ea30a95afb9b2c455ef0e2b6dd501802ea7
Revincx <revincx233@gmail.com>
2026-08-25 16:07:39 +0800

    chore: migrate nixos flake files

--- default.nix
+++ default.nix
@@ -1,10 +1,21 @@
-{ lib, tdesktop }:
+{ lib, telegram-desktop }:
 
-tdesktop.override {
-  pname = "yukigram";
-  unwrapped = tdesktop.unwrapped.overrideAttrs (f: {
-    name = "yukigram-unwrapped";
-    version = builtins.elemAt (builtins.match ".*(^|\n)AppVersionStrSmall *([^ ]*)($|\n).*" (builtins.readFile Telegram/build/version)) 1;
+let
+  versionLine = lib.findFirst
+    (line: lib.hasPrefix "AppVersionStrSmall " line)
+    (throw "AppVersionStrSmall is missing from Telegram/build/version")
+    (lib.splitString "\n" (builtins.readFile ./Telegram/build/version));
+  version = lib.removePrefix "AppVersionStrSmall " versionLine;
+in
+telegram-desktop.override {
+  pname = "yurigram";
+  unwrapped = telegram-desktop.unwrapped.overrideAttrs (old: {
+    pname = "yurigram-unwrapped";
+    inherit version;
     src = ./.;
+    meta = old.meta // {
+      description = "Yurigram, a Telegram Desktop client";
+      mainProgram = "Telegram";
+    };
   });
 }
--- flake.nix
+++ flake.nix
@@ -1,10 +1,49 @@
 {
-  description = "Yukigram";
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  outputs = { self, nixpkgs }: rec {
-    packages = builtins.mapAttrs (system: pkgs: rec {
-      yukigram = pkgs.callPackage ./default.nix {};
-      default = yukigram;
-    }) nixpkgs.legacyPackages;
+  description = "Yurigram";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    self.submodules = true;
   };
+
+  outputs = { self, nixpkgs }:
+    let
+      lib = nixpkgs.lib;
+      systems = [
+        "aarch64-linux"
+        "x86_64-linux"
+      ];
+      forAllSystems = lib.genAttrs systems;
+    in
+    {
+      packages = forAllSystems (
+        system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+        in
+        rec {
+          yurigram = pkgs.callPackage ./default.nix { };
+          default = yurigram;
+        }
+      );
+
+      apps = forAllSystems (
+        system:
+        let
+          package = self.packages.${system}.yurigram;
+          app = {
+            type = "app";
+            program = lib.getExe package;
+            meta = package.meta;
+          };
+        in
+        {
+          default = app;
+          yurigram = app;
+        }
+      );
+
+      formatter = forAllSystems (
+        system: nixpkgs.legacyPackages.${system}.nixfmt
+      );
+    };
 }
